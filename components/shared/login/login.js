14c68670d737ac8248bf49ec14acbdbfce964a77
Moreno Gentili <brightsoul@tiscali.it>
2018-06-26 21:21:33 +0200

    Styling login

--- components/shared/login/login.js
+++ components/shared/login/login.js
@@ -2,6 +2,7 @@ define(['vendor/knockout', 'services/identity'], function(ko, identity) {
 
     return function LoginViewModel() {
         this.password = ko.observable("");
+        this.username = ko.observable("");
         this.login = function() {
             if (this.password() == "password") {
                 this.password("");
